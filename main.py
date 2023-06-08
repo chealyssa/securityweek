3540d66edf36173a939a668317e10fdf648f323e
chealyssa <81701327+chealyssa@users.noreply.github.com>
2023-06-08 04:46:36 +0000

    learn how to read and minipulate files

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-names = ["Ash", "David", "Elena", "Karen"]
-places = {
-    "david" : "Earth",
-    "jim" : "Mars",
-    "Ken" : "Pluto" 
-}
-
-seperator = "/"
-print(seperator.join(names))
-
-print(seperator.join(places),"are in", seperator.join(places.values()))
+with open("apache_access.bib", "r") as file:
+    for line in file:
+        elements = line.split(" ")
+        transaction = {
+            'ip' : elements[0],
+            'timestamp' : ("".join([elements[3].strip("[]"),elements[4].strip("[]")])),
+            'kind_of_petition' : elements[5],
+            'route' : elements[6],
+            'version' : elements[7].strip('"'),
+            'status' : elements[8],
+            'completed_time' : elements[9].strip('\n')
+            }
+        
+        print(transaction)
